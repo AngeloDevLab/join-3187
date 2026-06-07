0e526918ca61368fe21e9bd91d7d248466158646
ChristianMulugeta <christiantatek.m@gmail.com>
2026-06-08 01:18:05 +0200

    Implement contact list rendering and alphabetical grouping

--- js/pages/contacts.js
+++ js/pages/contacts.js
@@ -7,29 +7,80 @@ const contacts = [
         color: "bg-orange"
     },
     {
+        name: "Anja Schulz",
+        email: "anja@gmail.com",
+        phone: "+49 123 456789",
+        initials: "AS",
+        color: "bg-purple"
+    },
+    {
         name: "Benedikt Ziegler",
         email: "benedikt@gmail.com",
         phone: "+49 123 456789",
         initials: "BZ",
         color: "bg-blue"
+    },
+    {
+        name: "David Eisenberg",
+        email: "david@gmail.com",
+        phone: "+49 123 456789",
+        initials: "DE",
+        color: "bg-orange"
+    },
+    {
+        name: "Eva Fischer",
+        email: "eva@gmail.com",
+        phone: "+49 123 456789",
+        initials: "EF",
+        color: "bg-purple"
     }
 ];
 
-// Initializes the contacts page by rendering the contacts list.
+// Initializes the contacts page by rendering the contacts list
 function init() {
+    contacts.sort((a, b) => a.name.localeCompare(b.name));
     renderContacts();
 }
 
-// Render all contacts
+// Render all contacts 
 function renderContacts() {
     let container = document.getElementById("contactsContainer");
     container.innerHTML = "";
+    let groupedContacts = {};
     for (let i = 0; i < contacts.length; i++) {
-        container.innerHTML += getContactTemplate(contacts[i]);
+        let contact = contacts[i];
+        let firstLetter = contact.name.charAt(0);
+        if (!groupedContacts[firstLetter]) {
+            groupedContacts[firstLetter] = [];
+        }
+        groupedContacts[firstLetter].push(contact);
+    }
+
+    renderGroupedContacts(groupedContacts, container);
+}
+
+// Render contacts grouped by their first letter
+function renderGroupedContacts(groupedContacts, container) {
+    for (let letter in groupedContacts) {
+        container.innerHTML += `
+            <div class="contact-group">
+                <h3>${letter}</h3>
+                ${getContactsByLetter(groupedContacts[letter])}
+            </div>
+        `;
     }
 }
 
-// Returns the HTML template for contact card.
+// Returns the HTML for a group of contacts that share the same first letter
+function getContactsByLetter(contactsArray) {
+    let html = "";
+    for (let i = 0; i < contactsArray.length; i++) {
+        html += getContactTemplate(contactsArray[i]);
+    }
+    return html;
+}
+
+// Returns the HTML template for contact card
 function getContactTemplate(contact) {
     return `
         <div class="contact-card">
